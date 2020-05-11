30e88e343b0bf1e0b29ce5170c00e71fdf3f1444
Александр <lvar-8@ya.ru>
2020-05-11 13:33:06 +0300

    Рабил по шаблонам

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
+import os
 from django.urls import path
 from core import views
 
 
+app_name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('products/', views.products, name='products​'),
-    path('contact/', views.contact, name='contact​'),
+    path('products/', views.products, name='products'),
+    path('contact/', views.contact, name='contact'),
 ]
