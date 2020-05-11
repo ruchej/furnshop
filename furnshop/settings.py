7dcbf92f70996cb666fe2e96126298af7bfad633
Александр Виноградов <lvar-8@ya.ru>
2020-05-11 10:55:42 +0300

    Какие-то изменения

--- furnshop/settings.py
+++ furnshop/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.apps.CoreConfig'
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +126,27 @@ FILE_CHARSET = 'utf-8'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'public_html', 'media')
 MEDIA_URL = '/media/'
+
+if DEBUG:
+    # INSTALLED_APPS += ['django_extensions']
+    # Enable debug_toolbar and template_profiler_panel
+    INSTALLED_APPS += ['debug_toolbar', ]
+    INTERNAL_IPS = ['127.0.0.1']
+    if 'debug_toolbar' in INSTALLED_APPS:
+        # debug_toolbar
+        MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+        DEBUG_TOOLBAR_PANELS = [
+            'debug_toolbar.panels.versions.VersionsPanel',
+            'debug_toolbar.panels.timer.TimerPanel',
+            'debug_toolbar.panels.settings.SettingsPanel',
+            'debug_toolbar.panels.headers.HeadersPanel',
+            'debug_toolbar.panels.request.RequestPanel',
+            'debug_toolbar.panels.sql.SQLPanel',
+            'debug_toolbar.panels.templates.TemplatesPanel',
+            'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+            'debug_toolbar.panels.cache.CachePanel',
+            'debug_toolbar.panels.signals.SignalsPanel',
+            'debug_toolbar.panels.logging.LoggingPanel',
+            'debug_toolbar.panels.redirects.RedirectsPanel',
+            'debug_toolbar.panels.profiling.ProfilingPanel',
+        ]
